c0556cf1d3292aeacc2a59b4b9a329feb00d0158
LSM-CODE <soriano250607@gmail.com>
2025-02-06 18:37:18 -0400

    Tarea 2

---
+++ Tarea 2/Tarea 2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicitar un número al usuario
+        Console.Write("Introduce un número: ");
+        int numero = Console.Read();
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El número ingresado es par.");
+        }
+        else
+        {
+            Console.WriteLine("El número ingresado es impar.");
+            
+        }
+        Console.ReadKey();
+    }
+}
